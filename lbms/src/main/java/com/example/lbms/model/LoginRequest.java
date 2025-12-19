5c6fcf4e5d7883e2786d7cc327658320afe0dd07
pooja02082020 <lohanipooja77@gmail.com>
2025-12-19 08:33:18 -0500

    Refactored

--- lbms/src/main/java/com/example/lbms/model/LoginRequest.java
+++ lbms/src/main/java/com/example/lbms/model/LoginRequest.java
@@ -5,13 +5,6 @@ public class LoginRequest {
     private String username;
     private String password;
  
-    public LoginRequest() {}
- 
-    public LoginRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
- 
     public String getUsername() {
         return username;
     }
