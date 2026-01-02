2610c5f7db9941c0345712ca7d3b6308e38d84a7
pooja02082020 <lohanipooja77@gmail.com>
2026-01-02 15:09:45 -0500

    added member entity and enum

---
+++ lbms/src/main/java/com/example/lbms/model/Member.java
@@ -0,0 +1,78 @@
+package com.example.lbms.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status; // ACTIVE, BLOCKED
+
+    private LocalDate joinedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public MemberStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(MemberStatus status) {
+		this.status = status;
+	}
+
+	public LocalDate getJoinedDate() {
+		return joinedDate;
+	}
+
+	public void setJoinedDate(LocalDate joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+    
+    
+}
+
